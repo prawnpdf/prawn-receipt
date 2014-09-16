7474a096c287e342f54a75894a3642610b2e6fef
David Ruan <ruanwz@gmail.com>
2014-09-16 21:14:14 +0800

    reorgnize spec and remove redundent code

--- spec/prawn/receipt_spec.rb
+++ spec/prawn/receipt_spec.rb
@@ -1,6 +1,6 @@
-require_relative '../../spec_helper'
+require_relative '../spec_helper'
 
-require_relative '../../../lib/prawn/receipt'
+require_relative '../../lib/prawn/receipt'
 
 describe Prawn::Receipt do
   before do
